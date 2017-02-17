b997b12fe64f41ac4d1a3f002544dd748f8acb54
morty <alemv96@hotmail.com>
2017-02-16 20:48:56 -0400

    Delete FullStatus.java

--- FullStatus.java
+++
@@ -1,6 +0,0 @@
-public class FullStatus implements Status{
-  private String statusBox;
-
-  public FullStatus(String statusBox){this.statusBox = statusBox;}
-  public String getStatusBox(){return this.statusBox;}
-}
