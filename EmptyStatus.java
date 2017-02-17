c407427c3e705f4453ecfcf2aa69eb4fbc2f848b
morty <alemv96@hotmail.com>
2017-02-16 20:48:39 -0400

    Delete EmptyStatus.java

--- EmptyStatus.java
+++
@@ -1,6 +0,0 @@
-public class EmptyStatus implements Status{
-  private String statusBox;
-
-  public EmptyStatus(String statusBox){this.statusBox = statusBox;}
-  public String getStatusBox(){return this.statusBox;}
-}
